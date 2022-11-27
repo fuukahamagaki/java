12573339729e9416bb74d13c984dd770427162eb
fuuka hamagaki <sw.frim@gmail.com>
2022-11-27 03:14:09 +0000

    introduceOneself

--- lesson.java
+++ lesson.java
@@ -1,12 +1,16 @@
 public class lesson {
-  public static void main(String[] args) {
-    System.out.println("メソッドを呼び出します");
-    hello("湊");
-    hello("朝香");
-    hello("菅原");
-    System.out.println("メソッドの呼び出しが終わりました");
+  public static void main(String[] args){
+    introduceOneself();
   }
-  public static void hello(String name) {
-    System.out.println(name + "さん、こんにちは");
+  
+  public static void introduceOneself() {
+    String name = "Minato";
+    int age = 22;
+    double height = 169.9;
+    char zodiac = '辰';
+    System.out.println("私の名前は" + name + "です");
+    System.out.println("歳は" + age + "です");
+    System.out.println("身長は" + height + "cmです");
+    System.out.println("十二支は" + zodiac + "です");
   }
 }
