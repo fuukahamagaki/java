4f4ca2ba887411d97d82ceae99d7ad09c3feb400
fuuka hamagaki <sw.frim@gmail.com>
2022-12-06 07:08:43 +0000

    練習

--- sample.java
+++ sample.java
@@ -1,12 +1,12 @@
-public class sample {
-  public static void main(String[] args) {
-    System.out.println("RPG:スッキリ魔王征伐");
-    System.out.println("Ver.0.1 by 湊");
-    System.out.println("<ただいま鋭意学習・制作中>");
-    System.out.println("プログラムを終了します");
-    int sum = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+package comment;
 
-    System.out.format("The sum of %s and %s is %s.\n",
-      args[0], args[1], Integer.toString(sum));
+public class sample {
+  public static void callDeae() {
+    System.out.println("えぇい、小癪な。曲者だ！出会えい！");
+  }
+  public static void showMondokoro() throws Exception {
+    System.out.println("飛車さん、角さん。もういいでしょう。");
+    System.out.println("この紋所が目に入らぬか！");
+    lesson.doTogame();
   }
 }
