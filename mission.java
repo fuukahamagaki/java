029319175544a6ea51d1678609d190c7cd37ab17
fuuka hamagaki <sw.frim@gmail.com>
2022-12-18 01:01:08 +0000

    int,String型

--- mission.java
+++ mission.java
@@ -1,5 +1,15 @@
-public class mission {
-  public static void main (String[] args) {
-    System.out.println("ありがとう");
+class mission {
+  public static void main(String[] args) {
+    
+    int number1 = 3;
+    int number2 = 7;
+    
+    System.out.println(number1 * number2);
+    
+    String text;
+    text = "プログラミングを勉強しよう";
+    
+    System.out.println("今から" + text);
+    
   }
 }
